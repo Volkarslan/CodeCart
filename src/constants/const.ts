90e533231cc323531fab53f6ca20bbde732ac995
Volkan Uğur ARSLAN <volkanugurarslan@gmail.com>
2024-12-08 20:42:20 +0300

    [F-5] Deploy Test 3

--- src/constants/const.ts
+++ src/constants/const.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_CATEGORY = "All";
 export const ITEMS_PER_PAGE = 12;
-export const MAX_PAGES = 2;
+export const MAX_PAGES = 4;
 
 export const TOAST_AUTO_CLOSE = 2000;
 export const TOAST_POSITION = "top-left";
